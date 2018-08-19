45d419112614a77baba2410959c49e501941062e
TamirShalit <shalit.tamir@gmail.com>
2018-08-20 00:04:05 +0300

    Fix doc

--- web_result_server/models/item.py
+++ web_result_server/models/item.py
@@ -4,7 +4,7 @@ from web_result_server.models.base import Base
 
 
 class TestItem(Base):
-    """Represents a result for a single test item."""
+    """Represents a single test item."""
     __tablename__ = 'items'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
