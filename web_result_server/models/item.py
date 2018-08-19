8b15237acb2fdf371ad42684592ab3c1538aad66
TamirShalit <shalit.tamir@gmail.com>
2018-08-20 00:21:39 +0300

    Add columns to test item

--- web_result_server/models/item.py
+++ web_result_server/models/item.py
@@ -1,6 +1,16 @@
 import sqlalchemy
+import sqlalchemy_utils
+import enum
 
 from web_result_server.models.base import Base
+
+
+class PytestResult(enum.Enum):
+    PASSED = 1
+    FAILED = 2
+    SKIPPED = 3
+    XFAILED = 4
+    XPASSED = 5
 
 
 class TestItem(Base):
@@ -8,3 +18,6 @@ class TestItem(Base):
     __tablename__ = 'items'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    nodeid = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    result = sqlalchemy.Column(sqlalchemy_utils.ChoiceType(PytestResult), nullable=False)
+    duration = sqlalchemy.Column(sqlalchemy.Float)
