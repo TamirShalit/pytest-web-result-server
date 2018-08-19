a6c226da40d8cc245ed2e3dd2b72280061aa1ca0
TamirShalit <shalit.tamir@gmail.com>
2018-08-19 23:21:00 +0300

    Add setup.py and base package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytest-web-result-server',
+    version='0.1',
+    packages=[find_packages('web_result_server')],
+    url='https://github.com/TamirShalit/pytest-web-result-server',
+    license='',
+    author='tamir',
+    author_email='shalit.tamir@gmail.com',
+    description='Web server for viewing pytest results'
+)
