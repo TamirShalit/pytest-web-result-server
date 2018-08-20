bb5766f126305ef7263a58d687827727b8ce4c40
TamirShalit <shalit.tamir@gmail.com>
2018-08-20 23:58:59 +0300

    Add db object fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import os
 import pytest
 
 from web_result_server import app_factory
+from web_result_server.database import db as _db
 
 TEST_DB_PATH = os.path.join(os.path.dirname(__file__), 'tests.db')
 TEST_DB_URI = 'sqlite:///{db_path}'.format(db_path=TEST_DB_PATH)
@@ -16,3 +17,16 @@ def app():
     flask_app.config = TEST_APP_CONFIGURATION
     with flask_app.app_context():
         yield flask_app
+
+
+@pytest.fixture(scope='session')
+def db(app):
+    if os.path.exists(TEST_DB_PATH):
+        os.remove(TEST_DB_PATH)
+
+    _db.init_app(app)
+    _db.create_all()
+    yield _db
+
+    if os.path.exists(TEST_DB_PATH):
+        os.remove(TEST_DB_PATH)
