5481bbd455a3936bc1e4d3e6b3d1f30a2b4b923c
TamirShalit <shalit.tamir@gmail.com>
2018-08-21 01:00:26 +0300

    Suppress warning in falsk-sqlalchemy

--- web_result_server/app_factory.py
+++ web_result_server/app_factory.py
@@ -3,5 +3,6 @@ from flask import Flask
 
 def create_app(name):
     app = Flask(name)
-    app.config = {'SERVER_NAME': None, 'DEBUG': False}
+    # SQLALCHEMY_TRACK_MODIFICATIONS is to suppress warning in flask-sqlalchemy
+    app.config = {'SERVER_NAME': None, 'DEBUG': False, 'SQLALCHEMY_TRACK_MODIFICATIONS': False}
     return app
