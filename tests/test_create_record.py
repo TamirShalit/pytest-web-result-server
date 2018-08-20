1422c27dbd51fe002cec814f1fd11da8a56f926a
TamirShalit <shalit.tamir@gmail.com>
2018-08-21 00:42:03 +0300

    Add test for creating records

---
+++ tests/test_create_record.py
@@ -0,0 +1,24 @@
+import pytest
+
+from web_result_server.models import item
+
+
+@pytest.mark.parametrize('record_class, init_arguments', [
+    (item.TestItem, dict(nodeid='tests/test_foo.py::test_foo',
+                         state=item.ItemState.DID_NOT_START)),
+    (item.TestItem, dict(nodeid='tests/sanity/test_bar.py::test_bar',
+                         state=item.ItemState.PASSED,
+                         duration=0.132))
+])
+def test_create_record(db, record_class, init_arguments):
+    """
+    :param db.Model record_class: SQLAlchemy class of the record.
+    :param dict init_arguments: Keyword arguments for the construction of the record.
+                                This is basically the column names and their corresponding values.
+    """
+    record = record_class(**init_arguments)
+    db.session.add(record)
+    db.session.commit()
+    row_from_db = record_class.query.first()
+    for column_name, element_value in init_arguments.items():
+        assert getattr(row_from_db, column_name) == element_value
