faa12eaaf8fa5f2fc130b8e68c69120d465cb717
TamirShalit <shalit.tamir@gmail.com>
2018-08-21 00:59:26 +0300

    Use default config values in factory

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,14 +8,13 @@ from web_result_server.database import db as _db
 TEST_DB_PATH = os.path.join(os.path.dirname(__file__), 'tests.db')
 TEST_DB_URI = 'sqlite:///{db_path}'.format(db_path=TEST_DB_PATH)
 
-TEST_APP_CONFIGURATION = {'TESTING': True, 'SQLALCHEMY_DATABASE_URI': TEST_DB_URI,
-                          'SERVER_NAME': None, 'DEBUG': False}
+TEST_APP_CONFIGURATION = {'TESTING': True, 'SQLALCHEMY_DATABASE_URI': TEST_DB_URI}
 
 
 @pytest.fixture(scope='session')
 def app():
     flask_app = app_factory.create_app(__name__)
-    flask_app.config = TEST_APP_CONFIGURATION
+    flask_app.config.update(TEST_APP_CONFIGURATION)
     with flask_app.app_context():
         yield flask_app
 
--- web_result_server/app_factory.py
+++ web_result_server/app_factory.py
@@ -3,4 +3,5 @@ from flask import Flask
 
 def create_app(name):
     app = Flask(name)
+    app.config = {'SERVER_NAME': None, 'DEBUG': False}
     return app
