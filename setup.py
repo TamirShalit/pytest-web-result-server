d4fb259354718a5dec4ca1f724b180728096b955
TamirShalit <shalit.tamir@gmail.com>
2018-08-20 23:32:17 +0300

    Move tests outside of the package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pytest-web-result-server',
     version='0.1',
-    packages=find_packages(exclude=('web_result_server.tests',)),
+    packages=find_packages(exclude=['tests']),
     url='https://github.com/TamirShalit/pytest-web-result-server',
     install_requires=['flask-sqlalchemy', 'sqlalchemy-utils', 'enum34'],
     tests_requires=['pytest'],
