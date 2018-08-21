4ca166e71bddf057927da6fb99b6a2405a062da5
TamirShalit <shalit.tamir@gmail.com>
2018-08-22 02:54:52 +0300

    Add columns

--- web_result_server/models/session.py
+++ web_result_server/models/session.py
@@ -2,4 +2,7 @@ from web_result_server.database import db
 
 
 class Session(db.Model):
-    """Represetns a Pytest session."""
+    """Represents a Pytest session."""
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime)
+    duration = db.Column(db.Float)
