90c270ae99407f235d4b280890baef393433fea7
TamirShalit <shalit.tamir@gmail.com>
2018-08-24 23:00:43 +0300

    Split tests to item and session

--- tests/test_create_record.py
+++ tests/test_create_record.py
@@ -4,34 +4,34 @@ import pytest
 
 from web_result_server.models import item, pytest_session
 
-
 INITIAL_PYTEST_SESSION = pytest_session.PytestSession(duration=2.6, start_time=datetime.now())
 
 
-@pytest.fixture(autouse=True)
-def add_pytest_session(session):
-    """Add constant pytest session so that `TestItem` can link to."""
-    session.add(INITIAL_PYTEST_SESSION)
-    session.commit()
-
-
-@pytest.mark.parametrize('record_class, init_arguments', [
-    (item.TestItem, dict(nodeid='tests/test_foo.py::test_foo',
-                         state=item.ItemState.DID_NOT_START)),
-    (item.TestItem, dict(nodeid='tests/sanity/test_bar.py::test_bar',
-                         state=item.ItemState.PASSED,
-                         duration=0.132)),
-    (pytest_session.PytestSession, dict(duration=21.01, start_time=datetime.now()))
+@pytest.mark.parametrize('init_arguments', [
+    dict(nodeid='tests/test_foo.py::test_foo', state=item.ItemState.DID_NOT_START),
+    dict(nodeid='tests/sanity/test_bar.py::test_bar', state=item.ItemState.PASSED, duration=0.132)
 ])
-def test_create_record(session, record_class, init_arguments):
+def test_create_pytest_item(session, init_arguments):
+    session.add(INITIAL_PYTEST_SESSION)
+    record = item.TestItem(session=INITIAL_PYTEST_SESSION, **init_arguments)
+    session.add(record)
+    session.commit()
+    row_from_db = item.TestItem.query.filter_by(**init_arguments).first()
+    for column_name, element_value in init_arguments.items():
+        assert getattr(row_from_db, column_name) == element_value
+
+
+@pytest.mark.parametrize('init_arguments', [
+    dict(duration=21.01, start_time=datetime.now())
+])
+def test_create_pytest_session(session, init_arguments):
     """
-    :param db.Model record_class: SQLAlchemy class of the record.
     :param dict init_arguments: Keyword arguments for the construction of the record.
                                 This is basically the column names and their corresponding values.
     """
-    record = record_class(**init_arguments)
+    record = pytest_session.PytestSession(**init_arguments)
     session.add(record)
     session.commit()
-    row_from_db = record_class.query.filter_by(**init_arguments).first()
+    row_from_db = pytest_session.PytestSession.query.filter_by(**init_arguments).first()
     for column_name, element_value in init_arguments.items():
         assert getattr(row_from_db, column_name) == element_value
