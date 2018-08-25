55656f530b0571a8556a53a71b2406fc577b4c7e
TamirShalit <shalit.tamir@gmail.com>
2018-08-25 15:26:27 +0300

    Add test

--- tests/test_server_config/test_server_config.py
+++ tests/test_server_config/test_server_config.py
@@ -1,5 +1,7 @@
 import os
 
+import pytest
+
 from web_result_server import app_factory
 from web_result_server.app_factory import DB_URI_CONFIG_KEY
 
@@ -10,3 +12,8 @@ def test_load_config():
     os.environ[app_factory.CONFIG_LOCATION_ENVIRONMENT_VARIABLE] = _TEST_CONFIG_LOCATION
     app = app_factory.create_app(__name__)
     assert app.config[DB_URI_CONFIG_KEY] == 'sqlite:///not_exist'
+
+
+def test_exception_raised_when_no_db_uri():
+    with pytest.raises(ValueError):
+        app_factory.create_app(__name__)
