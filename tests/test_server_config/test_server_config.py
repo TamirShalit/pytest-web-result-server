a3c37145d94ae44bafb228b016789b7e6fec62e7
TamirShalit <shalit.tamir@gmail.com>
2018-08-26 22:36:51 +0300

    Extract fixture to prevent test affect each other

--- tests/test_server_config/test_server_config.py
+++ tests/test_server_config/test_server_config.py
@@ -8,8 +8,15 @@ from web_result_server.app_factory import DB_URI_CONFIG_KEY
 _TEST_CONFIG_LOCATION = os.path.join(os.path.dirname(__file__), 'test_config.json')
 
 
-def test_load_config():
+@pytest.fixture
+def use_test_config():
     os.environ[app_factory.CONFIG_LOCATION_ENVIRONMENT_VARIABLE] = _TEST_CONFIG_LOCATION
+    yield
+    os.unsetenv(app_factory.CONFIG_LOCATION_ENVIRONMENT_VARIABLE)
+
+
+@pytest.mark.usefixtures('use_test_config')
+def test_load_config():
     app = app_factory.create_app(__name__)
     assert app.config[DB_URI_CONFIG_KEY] == 'sqlite:///not_exist'
 
