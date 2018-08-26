d83f5d8884ba33d033ec5a834a00195c447f0fdb
TamirShalit <shalit.tamir@gmail.com>
2018-08-26 22:23:41 +0300

    Add config load

--- web_result_server/app_factory.py
+++ web_result_server/app_factory.py
@@ -11,4 +11,6 @@ def create_app(name):
     app = Flask(name)
     # SQLALCHEMY_TRACK_MODIFICATIONS is to suppress warning in flask-sqlalchemy
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    config_location = os.getenv(CONFIG_LOCATION_ENVIRONMENT_VARIABLE, DEFAULT_CONFIG_LOCATION)
+    app.config.from_json(config_location)
     return app
