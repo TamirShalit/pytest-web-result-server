35e63bd1020e93d8b9051836e9deef1aa6e27eda
TamirShalit <shalit.tamir@gmail.com>
2018-08-26 22:20:28 +0300

    Add default location const

--- web_result_server/app_factory.py
+++ web_result_server/app_factory.py
@@ -1,5 +1,8 @@
+import os
+
 from flask import Flask
 
+DEFAULT_CONFIG_LOCATION = os.path.expanduser('~/web_results_server.json')
 CONFIG_LOCATION_ENVIRONMENT_VARIABLE = 'WEB_RESULTS_SERVER_CONFIG'
 DB_URI_CONFIG_KEY = 'SQLALCHEMY_DATABASE_URI'
 
