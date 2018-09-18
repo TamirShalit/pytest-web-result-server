1e3fe30bb77a2c8daf92ba19c9c732a4a6d7650d
TamirShalit <shalit.tamir@gmail.com>
2018-09-19 02:05:34 +0300

    Rename parameter and variable

--- webresultserver/main.py
+++ webresultserver/main.py
@@ -7,15 +7,15 @@ from webresultserver.models.pytest_session import PytestSession
 
 
 def main():
-    app = app_factory.create_app(__name__)
-    with app.app_context():
-        _create_db(app)
-        _create_rest_api(app)
-    app.run()
+    flask_app = app_factory.create_app(__name__)
+    with flask_app.app_context():
+        _create_db(flask_app)
+        _create_rest_api(flask_app)
+    flask_app.run()
 
 
-def _create_db(app):
-    db.init_app(app)
+def _create_db(flask_app):
+    db.init_app(flask_app)
     db.create_all()
 
 
