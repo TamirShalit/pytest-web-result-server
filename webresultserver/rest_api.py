dd0b33758919e8cc8bf6957ae59820362d43ed96
TamirShalit <shalit.tamir@gmail.com>
2018-09-24 15:10:58 +0300

    Make all methods static

--- webresultserver/rest_api.py
+++ webresultserver/rest_api.py
@@ -13,7 +13,8 @@ api = flask_restful.Api(api_blueprint)
 
 
 class AddPytestSession(flask_restful.Resource):
-    def post(self):
+    @staticmethod
+    def post():
         new_pytest_session = PytestSession(start_time=datetime.now())
         db.session.add(new_pytest_session)
         db.session.commit()
@@ -21,8 +22,9 @@ class AddPytestSession(flask_restful.Resource):
 
 
 class AddTestItem(flask_restful.Resource):
-    def post(self, session_id, nodeid):
-        self._ensure_item_not_exist(nodeid, session_id)
+    @classmethod
+    def post(cls, session_id, nodeid):
+        cls._ensure_item_not_exist(nodeid, session_id)
         new_test_item = TestItem(session_id=session_id,
                                  nodeid=nodeid,
                                  state=ItemState.DID_NOT_START)
@@ -30,7 +32,8 @@ class AddTestItem(flask_restful.Resource):
         db.session.commit()
         return new_test_item.id
 
-    def _ensure_item_not_exist(self, nodeid, session_id):
+    @staticmethod
+    def _ensure_item_not_exist(nodeid, session_id):
         if db.session.query(db.exists().where(db.and_(TestItem.session_id == session_id,
                                                       TestItem.nodeid == nodeid))).scalar():
             error_message = 'nodeid "{nodeid}" already exists in session {session_id}'.format(
@@ -39,20 +42,23 @@ class AddTestItem(flask_restful.Resource):
 
 
 class ChangeTestItemState(flask_restful.Resource):
-    def put(self, item_id, state_name):
+    @classmethod
+    def put(cls, item_id, state_name):
         state_name = state_name.upper()
-        self._ensure_valid_state_name(state_name)
-        test_item = self._get_existing_test_item(item_id)
+        cls._ensure_valid_state_name(state_name)
+        test_item = cls._get_existing_test_item(item_id)
         test_item.state = getattr(ItemState, state_name)
         db.session.commit()
         return {'id': item_id, 'state': state_name}
 
-    def _ensure_valid_state_name(self, state_name):
+    @staticmethod
+    def _ensure_valid_state_name(state_name):
         if not hasattr(ItemState, state_name):
             error_message = 'No item state named "{state_name}"'.format(state_name=state_name)
             flask_restful.abort(http.HTTPStatus.BAD_REQUEST, error_message=error_message)
 
-    def _get_existing_test_item(self, item_id):
+    @staticmethod
+    def _get_existing_test_item(item_id):
         test_item = db.session.query(TestItem).filter_by(id=item_id).first()
         if test_item is None:
             error_message = 'Item with ID {id} does not exist.'.format(id=item_id)
